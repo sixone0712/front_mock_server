3aa54e1e9eb60c3009fba2fa69f0ec0e8c2958f0
Dean <sixone0712@gmail.com>
2020-10-20 20:02:09 +0900

    파일 다운로드 커밋

--- src/devices/index.js
+++ src/devices/index.js
@@ -3,6 +3,7 @@ const data = require('../../data/data');
 const api = new Router();
 
 api.get('/', (ctx) => {
+  console.log(ctx.request.method, ctx.request.URL.href);
   ctx.body = data.getDeviceInfo();
 });
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-const cors = require('@koa/cors');
 const Koa = require('koa');
 const Router = require('koa-router');
 const device = require('./devices');
 const files = require('./files');
+const cors = require('@koa/cors');
 
 const app = new Koa();
 const router = new Router();
 
-app.use(cors());
+
+var options = {
+  origin: '*',
+  exposeHeaders: "*"
+};
+
+app.use(cors(options));
 
 router.use('/service/api/devices', device.routes());
 router.use('/service/api/files', files.routes());
 
+
 app.use(router.routes()).use(router.allowedMethods());
+
 
 app.listen(3100, () => {
   console.log('Listening to port 3100');
